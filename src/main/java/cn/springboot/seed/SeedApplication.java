7172ca0a2822ac8b1abfa826b3edd79086277a25
ziming <zimingnee@163.com>
2019-09-25 11:36:04 +0800

    war包版本

--- src/main/java/cn/springboot/seed/SeedApplication.java
+++ src/main/java/cn/springboot/seed/SeedApplication.java
@@ -8,12 +8,12 @@ import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan(basePackages = "cn.springboot.seed.mapper")
-public class SeedApplication /*extends SpringBootServletInitializer*/{
-    // @Override
-    // protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-    //     //SeedApplication.class为带有@SpringBootApplication注解的启动类
-    //     return builder.sources(SeedApplication.class);
-    // }
+public class SeedApplication extends SpringBootServletInitializer{
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        //SeedApplication.class为带有@SpringBootApplication注解的启动类
+        return builder.sources(SeedApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(SeedApplication.class, args);
     }
