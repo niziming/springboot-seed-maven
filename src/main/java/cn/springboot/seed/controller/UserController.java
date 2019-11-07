542ef7c4e0b6e1926b73db744d5b1e9eca86e15b
niziming <zimingnee@163.com>
2019-11-07 14:45:10 +0800

    SpringBoot-Seed

--- src/main/java/cn/springboot/seed/controller/UserController.java
+++ src/main/java/cn/springboot/seed/controller/UserController.java
@@ -8,6 +8,7 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -21,19 +22,18 @@ public class UserController {
     @GetMapping("/list")
     @ApiOperation(value = "用户列表请求", notes = "返回用户列表")
     @ResponseBody
-    public List<User> selectAll(){
+    public List<User> selectAll() {
         return userService.selectAllUsers();
     }
 
     @ApiOperation(value = "跳转用户登录", notes = "登录界面")
     @GetMapping("/login")
-    public String Login(){
+    public String Login() {
         return "user/login";
     }
 
     @ApiOperation(value = "用户登录", notes = "用户名密码登录")
     @PostMapping("/login")
-    public String Login(User user){
     public ModelAndView Login(User user){
         ModelAndView modelAndView = new ModelAndView();
         User loginUser = userService.login(user);
@@ -42,8 +42,10 @@ public class UserController {
             modelAndView.addObject(loginUser);
             System.out.println(modelAndView);
             return modelAndView;
+        } else {
+            modelAndView.setViewName("404");
+            return modelAndView;
         }
-        modelAndView.setViewName("user/login");
-        return modelAndView;
     }
+
 }
