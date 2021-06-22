a88ad89ad5c251084759002a9da5b00c7f2e4384
niziming <zimingnee@163.com>
2021-06-22 17:09:22 +0800

    整合 tk.mybatis 与 druid 和 gen

---
+++ tk-mybatis/src/main/java/cn/zm/tk/mapper/AccountMapper.java
@@ -0,0 +1,9 @@
+package cn.zm.tk.mapper;
+
+import cn.zm.tk.common.mapper.BaseMapper;
+import cn.zm.tk.entity.Account;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface AccountMapper extends BaseMapper<Account> {
+}
